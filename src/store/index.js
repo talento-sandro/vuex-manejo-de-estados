a31cfb7b1c4b04ed251bf2735299f720b2e13fc2
Sandro <luiss.morar@gmail.com>
2020-04-27 19:14:47 -0400

    Acciones y mutaciones

--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,29 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    msg: 'Vuex desde cero.',
+    nombre: 'Grover',
+    apellido: 'Vasquez',
+    amigos: [],
+    amigo: null
   },
   mutations: {
+    addAmigo( state){
+      state.amigos= [state.amigo, ...state.amigos]
+    }
   },
   actions: {
+    addAmigoAction(context) {
+      context.commit('addAmigo');
+    }
+  },
+  getters: {
+    mensaje (state){
+      return state.msg;
+    },
+    nombreCompleto (state) {
+      return `${state.nombre} ${state.apellido}`
+    }
   },
   modules: {
   }
